4e07f87aea6d1f450769d914caed9ac8a02a98df
Alapan10008 <89271547+Alapan10008@users.noreply.github.com>
2022-06-30 12:43:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,7 @@ from turtle import pos
 from flask import send_file
 from flask import Flask,request,jsonify
 from flask_restful import Api,Resource
-from flask_sqlalchemy import SQLAlchemy
 import os 
-from datetime import datetime
 import soundfile
 import numpy as np
 import librosa as lib
@@ -18,7 +16,6 @@ import werkzeug
 
 app=Flask(__name__)
 api=Api(app)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'
 
 
 @app.route("/fft", methods=["POST"])
@@ -30,10 +27,10 @@ def fft():
             filename = werkzeug.utils.secure_filename(audio_file.filename)
          
             print(filename)
-            audio_file.save('audiofiles/'+filename)
-            wav_loc = str('audiofiles/'+filename)
+            audio_file.save(filename)
+            wav_loc = str(filename)
             audio,sr=lib.load(wav_loc)
-            os.remove('audiofiles/'+filename) 
+            os.remove(filename) 
             
             filename = filename.split(".")[0]
             cutoff = 300
@@ -52,19 +49,19 @@ def noisereduser():
             # if not audio_file:
             #     return "No audio uploaded",400
             filename = werkzeug.utils.secure_filename(audio_file.filename)
-            audio_file.save('audiofiles/'+filename)
-            wav_loc = str('audiofiles/'+filename)
+            audio_file.save(filename)
+            wav_loc = str(filename)
             audio,sr=lib.load(wav_loc)
-            os.remove('audiofiles/'+filename) 
+            os.remove(filename) 
             filename = filename.split(".")[0]
             cutoff = 300
             order = 2  
             nq=(0.5*sr)
             y = butter_lowpass_filter(audio, cutoff, sr, order,nq)
-            soundfile.write('clean_audio/'+filename+'.wav', y, sr)
+            soundfile.write(filename+'.wav', y, sr)
     
             return send_file(
-            'clean_audio/'+filename+'.wav',
+            filename+'.wav',
             mimetype="audio/wav", 
             as_attachment=True, 
             attachment_filename=filename+'.wav')
